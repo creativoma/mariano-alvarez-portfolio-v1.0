071b3731655dbf7557b1f7984bddc95be45df826
Mariano Álvarez <ma.marianoalvarez@gmail.com>
2023-06-22 04:33:35 +0200

    Se agregó toast informando última fecha de actualización

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://landing-with-astro.vercel.app/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }),react()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
