98b1cacccc278f9647a15ec39d0183dba3301872
Mariano Álvarez <ma.marianoalvarez@gmail.com>
2023-09-30 17:46:25 +0200

    Actualización de contenido

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
@@ -8,9 +7,6 @@ export default defineConfig({
   site: "https://landing-with-astro.vercel.app/",
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     react(),
   ],
   vite: {
